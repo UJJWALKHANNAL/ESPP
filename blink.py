1fd8e764abe20d2b1f89a659cceef32f61fed5c9
UJJWALKHANNAL <UJJWALKHANNAL@users.noreply.github.com>
2025-03-27 15:52:08 +0530

    Update blink.py

--- blink.py
+++ blink.py
@@ -1,12 +1,12 @@
 import machine
 import time
 
-# Configure Pin 0 as output
-pin = machine.Pin(0, machine.Pin.OUT)
+# Configure Pin 0 as output (built-in LED on most ESP modules)
+led = machine.Pin(0, machine.Pin.OUT)
 
-# Blink the LED indefinitely
-for i in range(1000000000):
-    pin.on()
+# Blink LED with 0.5s interval
+while True:
+    led.on()  # Turn LED ON
     time.sleep(0.5)
-    pin.off()
+    led.off()  # Turn LED OFF
     time.sleep(0.5)
